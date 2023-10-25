b85e5bc7501c8091cd68770bc130267aa7ea2aba
Rostov4anin <mmaksguy@gmail.com>
2023-10-25 19:55:21 +0300

    first commit

---
+++ main/background.js
@@ -0,0 +1,56 @@
+import path from 'path'
+import { app, ipcMain } from 'electron'
+import serve from 'electron-serve'
+import { createWindow } from './helpers'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+if (isProd) {
+  serve({ directory: 'app' })
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`)
+}
+
+let mainWindow; // Declare a variable to store the mainWindow
+
+; (async () => {
+  await app.whenReady()
+
+  mainWindow = createWindow('main', {
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  if (isProd) {
+    await mainWindow.loadURL('app://./home')
+  } else {
+    const port = process.argv[2]
+    await mainWindow.loadURL(`http://localhost:${port}/home`)
+    mainWindow.webContents.openDevTools()
+  }
+})()
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on('close-win', () => {
+  app.quit()
+})
+
+ipcMain.on('minimize-win', () => {
+  if (mainWindow) {
+    mainWindow.minimize();
+  }
+})
+
+ipcMain.on('maximize-win', () => {
+  if (mainWindow && mainWindow.isMaximized()) {
+    mainWindow.restore(); // Restore the window if it is minimized
+  } else if (mainWindow) {
+    mainWindow.maximize();
+  }
+})
---
+++ renderer/components/map.jsx
@@ -0,0 +1,5 @@
+export default function Map() {
+    return (
+        <div className='info-container w-[1560px] h-[930px] m-[50px] bg-green-500 rounded-[30px]'></div>
+    );
+}
