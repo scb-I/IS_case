9af2cf73f15ee88be57897bdc45b99c3afbc4957
Rostov4anin <mmaksguy@gmail.com>
2023-10-27 01:46:22 +0300

    change urls

--- Client/renderer/pages/home.jsx
+++ Client/renderer/pages/home.jsx
@@ -7,7 +7,7 @@ import Map from '../components/map';
 export default function Home() {
     const [showMediaPlayer, setShowMediaPlayer] = useState(true);
     const [currentSrc, setCurrentSrc] = useState(
-        'https://github.com/scb-I/IS_case/blob/Max/nonedetection.mp4'
+        'https://user-images.githubusercontent.com/84803793/278491837-1dc0e88c-c2f5-44ae-a8ab-daa158bc7693.mp4'
     ); // Состояние для хранения текущего src
 
     const handleBuildingPlanClick = () => {
@@ -35,7 +35,7 @@ export default function Home() {
                             <button
                                 onClick={() =>
                                     changeSrcClick(
-                                        'https://github.com/scb-I/IS_case/blob/Max/nonedetection.mp4'
+                                        'https://user-images.githubusercontent.com/84803793/278491837-1dc0e88c-c2f5-44ae-a8ab-daa158bc7693.mp4'
                                     )
                                 }
                                 className='button-cam'
@@ -50,7 +50,7 @@ export default function Home() {
                             <button
                                 onClick={() =>
                                     changeSrcClick(
-                                        'https://github.com/scb-I/IS_case/blob/Max/detection.mp4'
+                                        'https://user-images.githubusercontent.com/84803793/278491813-10abd39a-9d4f-43e2-abb2-c7189cf33a33.mp4'
                                     )
                                 }
                                 className='button-cam'
